5501df5a4af3b5074e31fb7b904e87a839a5d80e
AlokPrasanna <alokprasanna1104@gmail.com>
2024-02-10 19:21:02 +0530

    Completed Project

--- app/auth/signout/route.js
+++ app/auth/signout/route.js
@@ -2,17 +2,15 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function GET(req){
+export async function POST(req){
     const cookieStore = cookies();
     const supabase = createRouteHandlerClient({cookies: () => cookieStore});
 
-    const {searchParams} = new URL(req.url)
+    const {data: {session}} = await supabase.auth.getSession();
 
-    const code = searchParams.get('code')
-
-    if (code){
-        await supabase.auth.exchangeCodeForSession(code)
+    if (session){
+        await supabase.auth.signOut()
     }
 
-    return NextResponse.redirect(new URL('/watch-list', req.url))
+    return NextResponse.redirect(new URL('/', req.url), {status: 302})
 }
